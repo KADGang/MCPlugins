ea263c98dae1038efd42ea5b4039c66a7c23fc5f
KADGang <54164637+KADGang@users.noreply.github.com>
2020-09-28 18:22:17 +0800

    更新生成狼的测试beta
    使用/callPet命令即可生成一条属于自己的狼

--- AIPetTest/src/main/java/aitest/aipettest/AIPetTest.java
+++ AIPetTest/src/main/java/aitest/aipettest/AIPetTest.java
@@ -1,5 +1,12 @@
 package aitest.aipettest;
 
+import org.bukkit.World;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Cow;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.Wolf;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class AIPetTest extends JavaPlugin {
@@ -14,5 +21,23 @@ public final class AIPetTest extends JavaPlugin {
     public void onDisable() {
         // Plugin shutdown logic
         getLogger().info("AIPet v1.0已卸载");
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (command.getName().equalsIgnoreCase("callPet")) {
+            if (!(sender instanceof Player)) {
+                sender.sendMessage("这个指令只能让玩家使用。");
+            } else {
+                Player cmdSender = (Player) sender;
+                World world = cmdSender.getWorld();
+                Wolf wolf = (Wolf) world.spawnEntity(cmdSender.getLocation(), EntityType.WOLF);
+                wolf.setOwner(cmdSender);
+                wolf.setCustomNameVisible(true);
+                wolf.setCustomName(cmdSender.getName() + "的狼");
+            }
+            return true;
+        }
+        return false;
     }
 }
