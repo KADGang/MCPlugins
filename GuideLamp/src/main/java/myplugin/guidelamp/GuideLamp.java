19153b453b6d6b491b77ce1f52f914848373e0c0
hearwindeva <55865298+hearwindeva@users.noreply.github.com>
2020-10-06 19:02:13 +0800

    1.1
    指路

--- GuideLamp/src/main/java/myplugin/guidelamp/GuideLamp.java
+++ GuideLamp/src/main/java/myplugin/guidelamp/GuideLamp.java
@@ -4,16 +4,15 @@ import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitTask;
 
-public final class GuideLamp extends JavaPlugin {
+public class GuideLamp extends JavaPlugin {
 
     @Override
     public void onEnable() {
         getLogger().info("onEnable has been invoked!");
-
+        //Bukkit.getServer().
     }
 
     @Override
@@ -26,7 +25,8 @@ public final class GuideLamp extends JavaPlugin {
         if (sender instanceof Player && command.getName().equalsIgnoreCase("GuideLamp") || label.equalsIgnoreCase("GL")) {
             if (args[0].equalsIgnoreCase("to")) {
                 Player target = Bukkit.getServer().getPlayer(args[1]);
-                BukkitTask Task = new RunTask(((Player)sender).getLocation(),target.getLocation()).runTaskTimer(this, 20L, 20L);
+                BukkitTask Task = new RunTask(((Player)sender),target).runTaskTimer(this, 20L, 8L);
+                return true;
             }
         }
         return false;
--- GuideLamp/src/main/java/myplugin/guidelamp/RunTask.java
+++ GuideLamp/src/main/java/myplugin/guidelamp/RunTask.java
@@ -1,20 +1,44 @@
 package myplugin.guidelamp;
 
-import org.bukkit.Location;
+import org.bukkit.*;
+import org.bukkit.entity.Player;
+import org.bukkit.event.world.WorldEvent;
+import org.bukkit.material.Redstone;
 import org.bukkit.scheduler.BukkitRunnable;
-import org.bukkit.Particle.DustOptions
 
 public class RunTask extends BukkitRunnable {
-    final int LENTH=10;
-    Location My,target;
+    final int COUNT=2;
+    Player My,target;
 
-    public RunTask(Location my, Location target) {
+    public RunTask(Player my, Player target) {
         My = my;
         this.target = target;
     }
 
     @Override
     public void run() {
-        ParticleEffect.REDSTONE.display(new ParticleEffect.OrdinaryColor(Color.RED), loc, 50);
+        Location a = My.getLocation();
+        Location b = target.getLocation();
+        World.Environment WE =target.getWorld().getEnvironment();
+        Color ColoR=Color.YELLOW;
+        double distance = Math.sqrt((a.getX() - b.getX()) * (a.getX() - b.getX()) + (a.getY() - b.getY()) * (a.getY() - b.getY()) + (a.getZ() - b.getZ()) * (a.getZ() - b.getZ()));
+        double disX=a.getX()+17*(b.getX()-a.getX())/distance,
+                disY=a.getY()+17*(b.getY()-a.getY())/distance,
+                disZ=a.getZ()+17*(b.getZ()-a.getZ())/distance;
+        switch (WE)
+        {
+            case NORMAL:
+                ColoR= Color.GREEN;
+                break;
+            case NETHER:
+                ColoR= Color.RED;
+                break;
+            case THE_END:
+                ColoR= Color.PURPLE;
+                break;
+        }
+        Location spawnLoc = new Location(My.getWorld(),disX,disY,disZ);
+        if(distance>15)My.getWorld().spawnParticle(Particle.REDSTONE, spawnLoc, COUNT,0.2,0.2,0.2,new Particle.DustOptions(ColoR,5));
+        //My.sendMessage(disX+" "+disY+" "+disZ);
     }
 }
